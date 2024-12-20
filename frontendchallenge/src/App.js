14f11d9d9c70a4c8b6741ef7dfdda3a6d61b5779
tharun06 <muthoju.tharun@gmail.com>
2024-12-20 08:43:50 -0500

    Update App.js

--- frontendchallenge/src/App.js
+++ frontendchallenge/src/App.js
@@ -1,1 +1,3 @@
-test
+<Typography size="size_12" margin-bottom="size_4" color="text_secondary">
+  Displaying {(pageIndex * pageSize) + 1} - {Math.min((pageIndex + 1) * pageSize, totalCount)} of {totalCount} items
+</Typography>
